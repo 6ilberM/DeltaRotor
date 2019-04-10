84fd85b9aa51aa71d4cc5cbefdbe504cfbc1c2d7
Gilber Moreno <6ilberM@gmail.com>
2019-04-10 00:17:59 -0500

    04/10 un-finished Changes

--- Delta-Muse/Assets/Scripts/a_ProjBounce.cs
+++ Delta-Muse/Assets/Scripts/a_ProjBounce.cs
@@ -1,7 +1,4 @@
 using UnityEngine;
-
-
-
 [RequireComponent(typeof(BoxCollider2D))]
 
 public class a_ProjBounce : MonoBehaviour
@@ -29,9 +26,10 @@ public class a_ProjBounce : MonoBehaviour
     private void Start()
     {
         projectileCfilter = new ContactFilter2D();
-        projectileCfilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        projectileCfilter.SetLayerMask(LayerMask.GetMask("Interactable"));
     }
 
+    Collider2D[] colliderstemp = new Collider2D[10];
     private void Update()
 
     {
@@ -41,9 +39,24 @@ public class a_ProjBounce : MonoBehaviour
         int colliderCount = myCollider.OverlapCollider(projectileCfilter, colliders);
         for (int i = 0; i < colliderCount; i++)
         {
-//continue here
-// Do bounce angles next then PLEASE COMPLETE A LEVEL GOD DAMN HAHA...
 
+            // Do bounce angles next then PLEASE COMPLETE A LEVEL GOD DAMN HAHA...
+            if (colliderstemp[i] != colliders[i])
+            {
+                Debug.Log(colliders[i].gameObject.transform.rotation);
+
+
+                Vector3 holder = colliders[i].gameObject.transform.rotation.eulerAngles;
+                float f_holder = holder.z;
+
+                colliders[i].gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+                colliderstemp = colliders;
+
+            }
+            if (false == colliderstemp[i].IsTouching(myCollider))
+            {
+                colliderstemp[i] = null;
+            }
         }
 
         if (colliderCount > 0)
@@ -52,8 +65,4 @@ public class a_ProjBounce : MonoBehaviour
         }
     }
 
-    private void LateUpdate()
-    {
-
-    }
 }
--- Delta-Muse/Assets/Scripts/editmodeGridSnap.cs
+++ Delta-Muse/Assets/Scripts/editmodeGridSnap.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+
 [ExecuteInEditMode]
 public class editmodeGridSnap : MonoBehaviour
 {
     float snapvalue = 1;
     private void Update()
     {
-        float snapInverse = 1 / snapvalue;
-        float x, y;
-        // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
-        // so 1.45 to nearest .5 is 1.5
+        if (Application.isPlaying)
+        {
+            // code executed in play mode
+        }
+        else
+        {
+            float snapInverse = 1 / snapvalue;
+            float x, y;
+            // if snapValue = .5, x = 1.45 -> snapInverse = 2 -> x*2 => 2.90 -> round 2.90 => 3 -> 3/2 => 1.5
+            // so 1.45 to nearest .5 is 1.5
 
-//Smooth movement of tiles easy placement!
-        x = Mathf.Floor(transform.position.x);
-        y = Mathf.Floor(transform.position.y);
-        transform.position = new Vector3(x + .5f, y + .5f);
+            //Smooth movement of tiles easy placement!
+            x = Mathf.Floor(transform.position.x);
+            y = Mathf.Floor(transform.position.y);
+            transform.position = new Vector3(x + .5f, y + .5f);
+        }
     }
+
 }
